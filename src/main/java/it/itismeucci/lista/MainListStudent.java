14608cd66fa18aa2123162748be6488bfa1ddffa
Benncio09 <lorebencini13@outlook.com>
2024-10-09 10:44:56 +0200

    iterator

--- src/main/java/it/itismeucci/lista/MainListStudent.java
+++ src/main/java/it/itismeucci/lista/MainListStudent.java
@@ -1,6 +1,7 @@
 package it.itismeucci.lista;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MainListStudent {
 
@@ -10,15 +11,30 @@ public class MainListStudent {
         
         for (int i = 0; i < 10; i++) {
             slist.add(new Student("a" + i, "b"+i));
-            
             System.out.println(slist.get(i));
         }
 
-        System.out.println(slist.size());
+        Student s = slist.get(5);
 
+        System.out.println(slist.size());
         slist.remove(7);
-
         System.out.println(slist.size());
-        
+        slist.remove(s);
+        System.out.println(slist.size());
+
+        String name = "a0";
+        Iterator<Student> iterator = slist.iterator(); //istanzia un iterator
+        while (iterator.hasNext()) { //finchè ce ne sono ancora
+            Student stud = iterator.next(); // prendi l'elemento successivo
+            if (stud.getName().equals(name)) {
+                iterator.remove(); //rimuovi l'elemento dalla lista
+            }
+        }
+        System.out.println(slist.size());
+        for (int i = 0; i < 10; i++) {
+            slist.add(new Student("a" + i, "b"+i));
+            System.out.println(slist.get(i));
+        }
+
     }
 }
